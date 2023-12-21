b48bd7ec5afe94872309f543f54e16487a02ef15
Omar7-leb <125736709+Omar7-leb@users.noreply.github.com>
2023-12-21 12:20:05 +0200

    1st save

---
+++ BookShoppingCartMvcUI/Data/DbSeadercs.cs
@@ -0,0 +1,33 @@
+﻿using BookShoppingCartMvcUI.Constants;
+using Microsoft.AspNetCore.Identity;
+
+namespace BookShoppingCartMvcUI.Data
+{
+	public class DbSeadercs
+	{
+		public static async Task  SeedDefaultData(IServiceProvider service)
+		{
+			var userMgr = service.GetService<UserManager<IdentityUser>>();
+			var roleMgr = service.GetService<RoleManager<IdentityRole>>();
+			// adding some roles to db
+			await roleMgr.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+			await roleMgr.CreateAsync(new IdentityRole(Roles.User.ToString()));
+
+			// create admin user
+
+			var admin = new IdentityUser
+			{
+				UserName = "admin@gmail.com",
+				Email = "admin@gmail.com",
+				EmailConfirmed = true,
+			};
+
+			var UserInDb = await userMgr.FindByEmailAsync(admin.Email);
+			if (UserInDb is null)
+			{
+				await userMgr.CreateAsync(admin, "Admin@123");
+				await userMgr.AddToRoleAsync(admin, Roles.Admin.ToString());
+			}
+		}
+	}
+}
